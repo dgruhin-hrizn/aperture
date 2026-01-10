d782bd5cfc4c9b34ee749bb6258c2b0dae4d7003
Hrizn <dgruhin@hrizn.io>
2026-01-09 21:27:39 -0500

    bump to 0.1.8

--- apps/api/src/routes/health.ts
+++ apps/api/src/routes/health.ts
@@ -19,7 +19,7 @@ const healthRoutes: FastifyPluginAsync = async (fastify) => {
     const health: HealthResponse = {
       ok: dbConnected,
       name: 'Aperture',
-      version: '0.1.7',
+      version: '0.1.8',
       time: new Date().toISOString(),
       database: {
         connected: dbConnected,
